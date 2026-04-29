85a20cc694eaabb2a508896414a6c701a5a99d8a
Jacob Pell <pelljacoba@gmail.com>
2026-04-29 13:18:41 -0400

    Working well. Pushing to test for concept approval.

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -1,21 +1,24 @@
 import { Divider, Grid, Typography, useTheme } from "@mui/material";
-import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
-import type { JSX } from "react";
-import { useNavigate, type NavigateFunction } from "react-router";
+import {
+  AdvancedMarker,
+  APIProvider,
+  Map,
+  Marker,
+} from "@vis.gl/react-google-maps";
+import { useNavigate } from "react-router";
 
 export default function Location() {
   const navigate = useNavigate();
   const theme = useTheme();
+  const token = import.meta.env.VITE_GAPI_KEY!;
   const location = (
     <Grid
-      size={{ xs: 12, md: 4 }}
+      size={{ xs: 12, sm: 6, md: 4 }}
       sx={{
         px: 3,
         justifyContent: "center",
         alignContent: "start",
         pb: theme.breakpoints.only("xs") ? 3 : 0,
-        maxWidth: theme.breakpoints.only("sm") ? "60%" : "100%",
-        mx: theme.breakpoints.only("sm") ? "auto" : "0",
       }}
     >
       <Typography variant="h3" component={"h3"}>
@@ -36,16 +39,22 @@ export default function Location() {
   );
 
   const map = (
-    <Grid size={{ xs: 12, md: 4 }}>
-      <APIProvider apiKey={"AIzaSyCfBrObtOeivGWuM2hnefjJLioFpEiIJLg"}>
+    <Grid size={{ xs: 12, sm: 6, md: 4 }}>
+      <APIProvider apiKey={token}>
         <Map
-          style={{ width: "300px", height: "300px", margin: "0 auto" }}
+          style={{
+            width: "300px",
+            height: "300px",
+            margin: "0 auto",
+            padding: "0 1em",
+          }}
           defaultCenter={{ lat: 39.62146813390072, lng: -86.15762153193997 }}
           defaultZoom={17}
           gestureHandling="greedy"
+          mapId="423afd03b213ef8ea8393497"
           disableDefaultUI
         >
-          <Marker
+          <AdvancedMarker
             position={{ lat: 39.62146813390072, lng: -86.15762153193997 }}
             onClick={() => {
               navigate(
@@ -66,8 +75,6 @@ export default function Location() {
         justifyContent: "center",
         alignContent: "start",
         pt: theme.breakpoints.only("xs") ? 3 : 0,
-        maxWidth: theme.breakpoints.only("sm") ? "60%" : "100%",
-        mx: theme.breakpoints.only("sm") ? "auto" : "0",
       }}
     >
       <Typography variant="h3" component={"h3"}>
@@ -75,23 +82,26 @@ export default function Location() {
       </Typography>
       <Divider />
       <br />
-      <Typography variant="h5">Monday: 3pm - 10pm</Typography>
-      <Typography variant="h5">Tuesday: 3pm - 10pm</Typography>
-      <Typography variant="h5">Wednesday: Closed</Typography>
-      <Typography variant="h5">Thursday: 3pm - 10pm</Typography>
-      <Typography variant="h5">Friday: 3pm - 10pm</Typography>
-      <Typography variant="h5">Saturday/Sunday:</Typography>
-      <Typography variant="h5" sx={{ textIndent: "30px" }}>
-        12 Noon - 10pm
-      </Typography>
+      <Grid container sx={{ alignItems: "center" }}>
+        <Grid size={{ xs: 12, sm: 6, md: 12 }}>
+          <Typography variant="h5">Monday: 3pm - 10pm</Typography>
+          <Typography variant="h5">Tuesday: 3pm - 10pm</Typography>
+          <Typography variant="h5">Wednesday: Closed</Typography>
+        </Grid>
+        <Grid size={{ xs: 12, sm: 6, md: 12 }}>
+          <Typography variant="h5">Thursday: 3pm - 10pm</Typography>
+          <Typography variant="h5">Friday: 3pm - 10pm</Typography>
+          <Typography variant="h5">Saturday/Sunday:</Typography>
+          <Typography variant="h5" sx={{ textIndent: "30px" }}>
+            12 Noon - 10pm
+          </Typography>
+        </Grid>
+      </Grid>
     </Grid>
   );
 
   return (
     <Grid container id="main-cont" role="article" aria-label="location">
-      {/* {theme.breakpoints.down("sm")
-        ? [location, map, hours].map((el) => el)
-        : [location, hours, map].map((el) => el)} */}
       {location}
       {map}
       {hours}
