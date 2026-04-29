80bb60da4cb74316e1b77ae744977797728ee5a7
Jacob Pell <pelljacoba@gmail.com>
2026-04-29 17:26:38 -0400

    Remove soft

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test.describe('homepage feature testing', ()=>{
     test.beforeEach('open page', async ({page})=>{
         await page.goto('/')
-        await expect(async()=>{expect.soft(page).toHaveTitle('Ye Gamer\'s Guild')}).toPass()
+        await expect(async()=>{expect(page).toHaveTitle('Ye Gamer\'s Guild')}).toPass()
     });
 
     test.afterEach('close page', async ({page})=>{
